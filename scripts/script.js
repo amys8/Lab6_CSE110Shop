4af95b12c1da865fcb790de884f0c45ef170142c
amys8 <60754183+amys8@users.noreply.github.com>
2021-02-11 22:26:24 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
+  var array;
   fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => array = data);
+  localStorage.setItem("products", JSON.stringify(array));
 });
