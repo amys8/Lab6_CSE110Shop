27bd44508ceb96ccd3ff3510b90a1c60ae1933bd
amys8 <60754183+amys8@users.noreply.github.com>
2021-02-12 20:46:29 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 // Script.js
 
 window.addEventListener('DOMContentLoaded', () => {
-  
   async function fetchData() {
     let response = await fetch('https://fakestoreapi.com/products');
     let data = await response.json()
@@ -19,7 +18,23 @@ window.addEventListener('DOMContentLoaded', () => {
     let node = document.createElement('product-item');
     node.shadowRoot.querySelector("img").src = i.image;
     node.shadowRoot.querySelector(".title").innerHTML = i.title; 
-    node.shadowRoot.querySelector(".price").innerHTML = i.price;
+    node.shadowRoot.querySelector(".price").innerHTML = "$" + i.price;
     document.getElementById("product-list").appendChild(node);
   }
 });
+
+let x = document.getElementById("product-list");
+let but = x.getElementsByTagName("button");
+but.addEventListener("click", buttonFunc);
+
+function buttonFunc() {
+  if (but.innerHTML === 'Add to Cart') {
+    but.innerHTML = 'Remove from Cart';
+    document.getElementById("cart-count").innerHTML = parseInt(document.getElementById("cart-count").innerHTML)+1;
+  }
+  else {
+    but.innerHTML = 'Add to Cart';
+    document.getElementById("cart-count").innerHTML = parseInt(document.getElementById("cart-count").innerHTML)-1;
+  }
+}
+    
