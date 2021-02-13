25916ac8c4b20f9629e63a4312b79b01464ba5b9
amys8 <60754183+amys8@users.noreply.github.com>
2021-02-13 14:05:49 -0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -19,7 +19,11 @@ window.addEventListener('DOMContentLoaded', () => {
     node.shadowRoot.querySelector("img").src = i.image;
     node.shadowRoot.querySelector(".title").innerHTML = i.title; 
     node.shadowRoot.querySelector(".price").innerHTML = "$" + i.price;
+    node.shadowRoot.querySelector("button").id = i.id;
     node.shadowRoot.querySelector("button").addEventListener("click", buttonFunc);
+    if (i.id in localStorage) {
+      node.shadowRoot.querySelector("button").buttonFunc();
+    }
     document.getElementById("product-list").appendChild(node);
   }
 });
@@ -28,11 +32,13 @@ function buttonFunc() {
   if (this.innerHTML === 'Add to Cart') {
     this.innerHTML = 'Remove from Cart'; 
     this.onclick = function() {alert('Removed from Cart!');};
+    localStorage.setItem(this.id, this.id);
     document.getElementById("cart-count").innerHTML = parseInt(document.getElementById("cart-count").innerHTML)+1;
   }   
   else {
     this.innerHTML = 'Add to Cart'; 
     this.onclick = function() {alert('Added to Cart!');};
+    localStorage.removeItem(this.id);
     document.getElementById("cart-count").innerHTML = parseInt(document.getElementById("cart-count").innerHTML)-1;
   } 
 }
